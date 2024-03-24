7447bf9dcc0928bf34cad325ba7ce213bf35abf8
Kamil Kwiatkowski <kamilkwiatkos@gmail.com>
2024-03-24 17:39:29 +0100

    Added unity project with visualization
    Program.cs became GraphGenerator.cs

---
+++ Assets/Scripts/GraphVisualConnection.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraphVisualConnection
+{
+    public LineRenderer lineRenderer;
+    public GameObject gameObject;
+    public Joint2D joint;
+}
